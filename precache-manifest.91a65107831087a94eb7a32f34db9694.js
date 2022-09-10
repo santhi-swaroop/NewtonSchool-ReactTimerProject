ebe97193047ea2c0bda471eddf835e3bb1dcf730
santhi-swaroop <62936115+santhi-swaroop@users.noreply.github.com>
2022-09-10 22:52:46 +0530

    Updates

---
+++ precache-manifest.91a65107831087a94eb7a32f34db9694.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "47c4353dd9de573be364",
+    "url": "/NewtonSchool-ReactTimerProject/static/css/main.aa116adc.chunk.css"
+  },
+  {
+    "revision": "47c4353dd9de573be364",
+    "url": "/NewtonSchool-ReactTimerProject/static/js/main.47c4353d.chunk.js"
+  },
+  {
+    "revision": "48dc1c1c87d8faf3dbc4",
+    "url": "/NewtonSchool-ReactTimerProject/static/js/1.48dc1c1c.chunk.js"
+  },
+  {
+    "revision": "873801dfef047642b51c",
+    "url": "/NewtonSchool-ReactTimerProject/static/js/runtime~main.873801df.js"
+  },
+  {
+    "revision": "6d0be2087711143ff000de10c0082c15",
+    "url": "/NewtonSchool-ReactTimerProject/index.html"
+  }
+];
